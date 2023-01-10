c6e03789dfefe3dceb9f02425e75d07207d23b7a
Alireza Rezaie (majnoon) <majnoonrze@outlook.com>
2023-01-10 17:53:58 +0330

    Add Replace All Folders Names Or Files Names in Directory App in project

--- Rename Files And Folders/Form1.Designer.cs
+++ Rename Files And Folders/Form1.Designer.cs
@@ -29,33 +29,120 @@ namespace Rename_Files_And_Folders
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(630, 22);
+            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Image = global::Rename_Files_And_Folders.Properties.Resources.Folder;
+            this.button1.Location = new System.Drawing.Point(648, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(152, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(23, 23);
+            this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(578, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source Text:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(578, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Change To:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 98);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(331, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Folders";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(349, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(322, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Files";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(684, 131);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(700, 170);
+            this.MinimumSize = new System.Drawing.Size(700, 170);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Replace All Folders Name Or Files Name In Directory";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Rename Files And Folders/Form1.cs
+++ Rename Files And Folders/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Rename_Files_And_Folders
@@ -17,5 +11,38 @@ namespace Rename_Files_And_Folders
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
+            if (directchoosedlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = directchoosedlg.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dirAa = new DirectoryInfo(textBox1.Text);
+
+            foreach (var dir in dirAa.GetDirectories())
+            {
+                if (!dir.Name.Contains(textBox2.Text)) continue;
+                string name = dir.Name.Replace(textBox2.Text, textBox3.Text);
+                var newpath = dirAa.FullName + "\\" + name;
+                Directory.Move(dir.FullName, newpath);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dirAa = new DirectoryInfo(textBox1.Text);
+            foreach (var dir in dirAa.GetFiles())
+            {
+                if (!dir.Name.Contains(textBox2.Text)) continue;
+                var name = dir.Name.Replace(textBox2.Text, textBox3.Text);
+                var newpath = dirAa.FullName + "\\" + name;
+                Directory.Move(dir.FullName, newpath);
+            }
+        }
     }
 }
