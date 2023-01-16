c5db32d4917a5df561fa80cbd9e365f96d3ed14f
Alireza Rezaie (majnoon) <majnoonrze@outlook.com>
2023-01-16 15:47:55 +0330

    - Bug Fixed When Rename "." in File Name

--- Rename Files And Folders/Form1.cs
+++ Rename Files And Folders/Form1.cs
@@ -13,35 +13,55 @@ namespace Rename_Files_And_Folders
 
         private void button1_Click(object sender, EventArgs e)
         {
-            FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
-            if (directchoosedlg.ShowDialog() == DialogResult.OK)
+            try
             {
-                textBox1.Text = directchoosedlg.SelectedPath;
+                FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
+                if (directchoosedlg.ShowDialog() == DialogResult.OK)
+                {
+                    textBox1.Text = directchoosedlg.SelectedPath;
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(@"Oh! Sometighns wrongs");
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            var dirAa = new DirectoryInfo(textBox1.Text);
-
-            foreach (var dir in dirAa.GetDirectories())
+            try
             {
-                if (!dir.Name.Contains(textBox2.Text)) continue;
-                string name = dir.Name.Replace(textBox2.Text, textBox3.Text);
-                var newpath = dirAa.FullName + "\\" + name;
-                Directory.Move(dir.FullName, newpath);
+                var dirAa = new DirectoryInfo(textBox1.Text);
+                foreach (var dir in dirAa.GetDirectories())
+                {
+                    if (!dir.Name.Contains(textBox2.Text)) continue;
+                    string name = dir.Name.Replace(textBox2.Text, textBox3.Text);
+                    var newpath = dirAa.FullName + "\\" + name ;
+                    Directory.Move(dir.FullName, newpath);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(@"Oh! Sometighns wrongs");
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            var dirAa = new DirectoryInfo(textBox1.Text);
-            foreach (var dir in dirAa.GetFiles())
+            try
             {
-                if (!dir.Name.Contains(textBox2.Text)) continue;
-                var name = dir.Name.Replace(textBox2.Text, textBox3.Text);
-                var newpath = dirAa.FullName + "\\" + name;
-                Directory.Move(dir.FullName, newpath);
+                var dirAa = new DirectoryInfo(textBox1.Text);
+                foreach (var dir in dirAa.GetFiles())
+                {
+                    if (!Path.GetFileNameWithoutExtension(dir.Name).Contains(textBox2.Text)) continue;
+                    var name = Path.GetFileNameWithoutExtension(dir.Name).Replace(textBox2.Text, textBox3.Text);
+                    var newpath = dirAa.FullName + "\\" + name+ Path.GetExtension(dir.Name);
+                    Directory.Move(dir.FullName, newpath);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(@"Oh! Sometighns wrongs");
             }
         }
     }
